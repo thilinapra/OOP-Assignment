33585e607a79d9789eb967d0ffbfab45fb0f9ab4
thilinapra <163219562+thilinapra@users.noreply.github.com>
2024-03-14 22:10:05 +0530

    Add files via upload

---
+++ Inky.java
@@ -0,0 +1,52 @@
+public class Inky{
+
+    void Inky(String donky)
+
+    {
+
+    System.out.println(donky +"\n");
+    
+
+
+
+    }
+
+
+    void Inky(String donky,String pinky,String monkey)
+
+    {
+
+System.out.println(pinky +" "+ donky);
+System.out.println(donky + " "+ monkey);
+System.out.println(pinky + " "+  donky " " + monkey);
+
+
+
+
+
+    }
+
+
+
+
+ public static void main(String[] args)
+
+ {
+
+String pinky="Donky";
+String blinky="Pinky";
+String rinky="Monkey";
+Inky ponky =new Inky();
+ponky.Inky(pinky);
+ponky.Inky(pinky,blinky,rinky);
+
+
+
+
+
+
+
+}
+
+
+}
